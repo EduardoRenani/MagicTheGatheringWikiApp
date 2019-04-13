eb6d15608188bef5da803f02bddcc2e3b4795dd9
Enrique Bozza Dutra <ebodutra@gmail.com>
2019-04-13 20:31:20 -0300

    cardsCellClass

---
+++ hearthstoneWiki/cardsTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  cardsTableViewCell.swift
+//  hearthstoneWiki
+//
+//  Created by Enrique Dutra on 13/04/19.
+//  Copyright © 2019 Eduardo Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class cardsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cardNameLabel: UILabel!
+    @IBOutlet weak var cardRarityLabel: UILabel!
+    @IBOutlet weak var cardTypeLabel: UILabel!
+    @IBOutlet weak var cardBioLabel: UILabel!
+    @IBOutlet weak var cardManaCountImageView: UIImageView!
+    @IBOutlet weak var cardImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
