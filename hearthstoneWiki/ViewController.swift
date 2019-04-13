b5f897cae27d292f883e2e9a7942e638ff302da3
Eduardo Renani <eduardo.renani@hotmail.com>
2019-04-13 15:49:25 -0300

    create a table view for all cards

--- hearthstoneWiki/ViewController.swift
+++ hearthstoneWiki/ViewController.swift
@@ -9,12 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let cardCellIdentifier = "cardIdentifier"
+    
+    var cards: [Card] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        DataService.getCardsByColor(cardColor: "white") { (cards) in
+            self.cards = cards ?? []
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
-
-
 }
 
+// MARK: - TableView DataSource
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cards.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cardCellIdentifier, for: indexPath)
+        
+        let card = cards[indexPath.row]
+
+        cell.textLabel?.text = card.name
+        cell.detailTextLabel?.text = card.rarity
+        
+        print(card.name)
+        print(card.rarity)
+        print(card.imagePath)
+        print(card.colors)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
