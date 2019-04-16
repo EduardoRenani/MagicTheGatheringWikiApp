f73c41335af807b2e5109bbe24f1a75f5726df1e
Eduardo Renani <eduardo.renani@hotmail.com>
2019-04-16 15:20:45 -0300

    logo created

---
+++ hearthstoneWiki/cardsNavigationBar.swift
@@ -0,0 +1,21 @@
+//
+//  cardsNavigationBar.swift
+//  hearthstoneWiki
+//
+//  Created by Eduardo Ribeiro on 16/04/19.
+//  Copyright © 2019 Eduardo Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class cardsNavigationBar: UINavigationBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
