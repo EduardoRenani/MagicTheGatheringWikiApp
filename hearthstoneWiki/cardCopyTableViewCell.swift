d82dec5fb3ebce7b21641e8d2b14cde476f3cfc2
Eduardo Renani <eduardo.renani@hotmail.com>
2019-04-17 14:21:17 -0300

    deck creation

---
+++ hearthstoneWiki/cardCopyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  cardCopyTableViewCell.swift
+//  hearthstoneWiki
+//
+//  Created by Eduardo Ribeiro on 17/04/19.
+//  Copyright © 2019 Eduardo Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class cardCopyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
